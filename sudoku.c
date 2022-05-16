b68bc15799bd27f3c9a121ac7d95d8e5082b7d2d
CARLOSAGUIRRE10 <>
2022-05-16 00:15:05 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -123,6 +123,10 @@ List* get_adj_nodes(Node* n)
           Node *adjNode = copy(n);
           adjNode->sudo[i][j] = k;
           pushBack(list,adjNode);
+          /*if(is_valid(adjNode) == 1)
+          {
+            pushBack(list, adjNode);
+          } rompe el codigo*/
         }
       }
     }
